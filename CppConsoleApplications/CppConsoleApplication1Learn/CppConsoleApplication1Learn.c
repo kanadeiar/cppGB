6caaa060050ef67c2d21414405d1385d6f088f2c
kanadeiar <kanadeiar@gmail.com>
2019-12-26 01:37:28 +0300

    end work

--- CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.c
+++ CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.c
@@ -1,48 +1,21 @@
-﻿
-
-#include <locale.h>
+﻿#include <locale.h>
 #include <stdio.h>
 #include <math.h>
 
-int getNum();
-void do_work();
-int pow(int base, int sign);
+#include "CppConsoleApplication1Learn.h"
 
+#define SIZE 10
 
-int main()
+int main(int argc, const char* argv[])
 {
 	setlocale(LC_ALL, "russian");
-	do_work();
-	int rez = pow(2,10);
-	printf_s("\nРезультат: %d", rez);
+	printf_s("Привет мир");
+
+	
 	
 	getchar();
+	return 0;
 }
 
-void do_work()
-{
-	int num = 71;
 
-	printf("\n");
-	int d = 0, i = 1;
-	while (i <= num)
-	{
-		if (num % i++ == 0)
-			d++;
-		else
-			continue;
-		if (d == 3) break;
-	}
-	printf_s("Введенное число %d %s является простым числом", num, (d==2) ? "" : "не ");
-}
-
-int pow(int base, int sign)
-{
-	int result = 1;
-	for (int i = 0; i<sign; i++)
-	{
-		result *= base;
-	}
-	return result;
-}
 
