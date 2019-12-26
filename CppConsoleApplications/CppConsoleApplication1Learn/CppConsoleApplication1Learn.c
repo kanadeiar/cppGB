66fafa4d28726193a231d5bcfff297476a036097
kanadeiar <kanadeiar@gmail.com>
2019-12-26 22:32:38 +0300

    end learn

--- CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.c
+++ CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.c
@@ -11,9 +11,17 @@ int main(int argc, const char* argv[])
 	setlocale(LC_ALL, "russian");
 	printf_s("Привет мир");
 
+	printf_s("Введите любое число:");
+	int a;
+	scanf_s(" %d", &a);
+	printf_s("\nСумма = %d\n", sum(a, 50));
+
+	int num = 71;
+	printf_s("Введите любое число:");
+	scanf_s(" %d", &num);	
+	printf_s("Введенное целое число %d %sявляется простым", num, isPrime(num)? "" : "не " );
 	
-	
-	getchar();
+
 	return 0;
 }
 
--- CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.h
+++ CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.h
@@ -1,3 +1,21 @@
 #pragma once
 
+int sum(int x, int y)
+{
+	int result = x + y;
+	return result;
+}
 
+int isPrime(int number)
+{
+	int d = 0, i =1;
+	while (i <= number)
+	{
+		if (number % i++ == 0)
+			d++;
+		else
+			continue;
+		if (d >= 3) return 0;
+	}
+	return (d == 2);
+}
