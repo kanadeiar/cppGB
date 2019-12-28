0bb8db6f560d425630c1e0d184ac29f40b175baf
kanadeiar <kanadeiar@gmail.com>
2019-12-28 17:12:12 +0300

    init

--- CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.c
+++ CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.c
@@ -2,23 +2,12 @@
 #include <windows.h>
 
 
-typedef struct Fact
-{
-	int one;
-	int two;
-};
-
-
 
 int main(int argc, const char* argv[])
 {
 	SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 	//HelloName();
-	struct Fact f1;
-	f1.one = 1;
-	f1.two = 9;
-	
 
 	
 	return 0;
