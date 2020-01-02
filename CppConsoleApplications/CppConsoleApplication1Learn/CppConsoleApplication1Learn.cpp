3c51846f0980193c5c24133cc613186d04c9b837
kanadeiar <kanadeiar@gmail.com>
2020-01-02 14:39:36 +0300

    add cpp

--- CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.cpp
+++ CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.cpp
@@ -1,15 +1,16 @@
 ﻿#include "CppConsoleApplication1Learn.h"
+#include <iostream>
 #include <windows.h>
+#include <iomanip>
 
-
-int main(int argc, const char* argv[])
+using namespace std;
+int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	/////////////////////////////////////////////////////
+	
 
-
-	/////////////////////////////////////////////////////
+	cin.get();
 	return 0;
 }
 
