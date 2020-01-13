70299bed985f73342232ba2a25b8846b405d4c8b
kanadeiar <kanadeiar@gmail.com>
2020-01-13 21:00:07 +0300

    create new project

--- CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.cpp
+++ CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.cpp
@@ -17,20 +17,9 @@ int main()
 	SetConsoleOutputCP(1251);
 	////////////////////////////////
 
-	const int n = 10;
-	int arr[n] = {};
-	int i = 10;
-	for (auto &element : arr)
-		element = i++;
-	int temp;
-	for (int i = 0; i < n/2; i++)
-	{
-		temp = arr[i];
-		arr[i] = arr[n - 1 - i];
-		arr[n - 1 - i] = temp;
-	}
 	
 
+	
 	
 	cin.get();
 	////////////////////////////////
---
+++ CppConsoleApplications/ProjectCLearn/Source.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include  <math.h>
+
+int main(int argc, char * args[])
+{
+
+	printf_s("Hello!");
+
+	getchar();
+}
+
