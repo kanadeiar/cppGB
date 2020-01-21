1ec440b7f16f33dac29e4a5acd945b697bb205f1
kanadeiar <kanadeiar@gmail.com>
2020-01-21 18:50:23 +0300

    add text

--- CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.cpp
+++ CppConsoleApplications/CppConsoleApplication1Learn/CppConsoleApplication1Learn.cpp
@@ -7,19 +7,41 @@
 #include <fstream>
 
 
+
 using namespace std;
 int main()
 {
 	Ru();
+	string s;
 
-
+	ifstream fin;
+	fin.open("data.txt");
+	if (fin)
+	{
+		int i = 1;
+		while (getline(fin, s))
+		{
+			int p = s.rfind(' ');
+			string st = s.substr(p + 1);
+			int balls = stoi(st);
+			if (balls >= 80)
+			{
+				st = s.substr(0, p);
+				cout << i++ << ") " << st << endl;
+			}
+			
+		}
+	}
+	else
+	{
+		cout << "Не открылся файл.";
+		cin.get();
+		exit(1);
+	}
 
 	
-	
+	fin.close();
 
-
-	
-	
 	cin.get();
 	////////////////////////////////
 	return 0;
--- CppConsoleApplications/ProjectCLearn/Source.c
+++ CppConsoleApplications/ProjectCLearn/Source.c
@@ -9,6 +9,8 @@
 int main(int argc, char * args[])
 {
 	Ru();
+
+
 	
 
 
