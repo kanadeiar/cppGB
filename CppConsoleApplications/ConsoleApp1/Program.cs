9e65c6a530cbe6ac9ae7b4d362ab97235412de15
kanadeiar <kanadeiar@gmail.com>
2020-04-11 11:03:48 +0300

    add delegate

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -8,16 +8,54 @@ using System.IO;
 
 namespace ConsoleApp1
 {
-
-
+    public delegate void MyDelegate(object o);
+    class Source
+    {
+        MyDelegate functions;
+        public void Add(MyDelegate f)
+        {
+            functions += f;
+        }
+        public void Remove(MyDelegate f)
+        {
+            functions -= f;
+        }
+        public void Run()
+        {
+            Console.WriteLine("RUN!");
+            functions?.Invoke(this);
+        }
+    }
+    class Observer1
+    {
+        public void Do(object o)
+        {
+            Console.WriteLine($"Первый принял {o} побежал");
+        }
+    }
+    class Observer2
+    {
+        public void Do(object o)
+        {
+            Console.WriteLine($"Второй принял {o} побежал");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-
-
-
+            Source s = new Source();
+            Observer1 o1 = new Observer1();
+            Observer2 o2 = new Observer2();
+            MyDelegate d1 = new MyDelegate(o1.Do);
+            s.Add(d1);
+            s.Add(o2.Do);
+            s.Run();
+            s.Remove(o1.Do);
+            s.Run();
             Console.ReadKey();
         }
+
+
     }
 }
