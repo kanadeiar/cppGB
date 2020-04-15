b425f857528c1a9599b1a385f58c6fe54ddfb8d0
kanadeiar <kanadeiar@gmail.com>
2020-04-15 17:32:36 +0300

    add linq

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -2,58 +2,160 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace ConsoleApp1
 {
-    class Element
+    public struct User
     {
-        private string fio;
-        private int ball;
-        public Element(string fio, int ball)
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public int Age {get;set;}
+        public User(string firstName, string lastName) : this()
         {
-            this.fio = fio;
-            this.ball = ball;
+            FirstName = firstName;
+            LastName = lastName;
         }
-        public string FIO => fio;
-        public int Ball => ball;
     }
+    public class ExampleLinq
+    {
+        private readonly User[] users;
+        private readonly int[] numbers;
+        public ExampleLinq()
+        {
+            this.users = new[]
+            {
+                new User("Roman","Muratov"){Age = 18},
+                new User("Ivan","Petrov"){Age = 22},
+                new User("Igor","Ivanov"){Age = 26},
+                new User("Sveta","Muratova"){Age = 16},
+                new User("Lena","Ivanova"){Age = 36},
+                new User("Leonid","Petrov"){Age = 25},
+                new User("Max","Muratov"){Age = 20},
+            };
+            this.numbers = new[]{1,2,3,4,9,15};
+        }
+        public void Filter()
+        {
+            var evens = numbers.Where(i=>i%2==0 && i>6);
+            foreach (var el in evens)
+                Console.WriteLine(el);
+        }
+        public void SelectComplex()
+        {
+            var selectUsers = from user in users
+                              where user.Age>20
+                              select user;
+            var selUsr1 = selectUsers.ToList();
+            foreach (var el in selUsr1)
+                Console.WriteLine($"{el.FirstName} - {el.LastName}");
+        }
+        public void Projection()
+        {
+            var names = users.Select(u => u.FirstName).ToList();
+            foreach (var user in names)
+                Console.WriteLine(user);
+        }
+        public void ExampleLet()
+        {
+            var people = from u in users
+                         let age = u.Age <= 18 ? u.Age + (18-u.Age) : u.Age
+                         select new User(u.FirstName, u.LastName)
+                         {
+                             Age = age
+                         };
+            foreach (var user in people)
+                Console.WriteLine($"{user.FirstName}-{user.LastName}");
+        }
+        public void SamplingFromTwo()
+        {
+            var people = from user in users
+                         from num in numbers
+                         select new {Name=user.LastName,Number=num};
+            foreach (var p in people)
+                Console.WriteLine($"{p.Name}-{p.Number}");
+        }
+        public void Sorting()
+        {
+            var sortedUsers = from u in users
+                              orderby u.Age
+                              select u;
+            foreach (var u in sortedUsers)
+                Console.WriteLine($"{u.FirstName}-{u.Age}");
+        }
+        public void WorkingWithSets()
+        {
+            string[] peopleOne = {"Igor","Roman","Ivan"};
+            string[] peopleTwo = {"Roman","Ivanovo","Den"};
+            var result1 = peopleOne.Except(peopleTwo);
+            var result2 = peopleOne.Intersect(peopleTwo);
+            var result3 = peopleOne.Union(peopleTwo);
+            var result4 = peopleOne.Concat(peopleTwo).Distinct();
+            foreach (var el in result1)
+                Console.Write($"{el} ");
+            Console.WriteLine();
+            foreach (var el in result2)
+                Console.Write($"{el} ");
+            Console.WriteLine();
+            foreach (var el in result3)
+                Console.Write($"{el} ");
+            Console.WriteLine();
+            foreach (var el in result4)
+                Console.Write($"{el} ");
+            Console.WriteLine();
+        }
+        public void Average()
+        {
+            double avr1 = numbers.Average();
+            Console.WriteLine($"{avr1}");
+            var arr1=(from i in numbers select i).Take(2);
+            foreach (var el in arr1)
+                Console.WriteLine($"{el}");
+            var arr2=(from i in numbers select i).Skip(2);
+            foreach (var el in arr2)
+                Console.WriteLine($"{el}");
+        }
+        public void Group()
+        {
+            //var groups = from user in users
+            //             group user by user.LastName;
+            //foreach (var g in groups)
+            //{
+            //    Console.Write($"{g.Key}: ");
+            //    foreach (var t in g)
+            //        Console.Write($"{t.FirstName} {t.Age} ");
+            //    Console.WriteLine();
+            //}
+            var group1 = users.GroupBy(p=>p.LastName)
+                .Select(g=>new
+                {
+                    Family = g.Key,
+                    Count = g.Count(),
+                    Names = g.Select(p=>p),
+                });
+            foreach (var el in group1)
+            {
+                Console.Write($"{el.Family} {el.Count} шт.: ");
+                foreach (var t in el.Names)
+                    Console.Write($"{t.FirstName} ");
+                Console.WriteLine();
+            }
+
+        }
+
+
+
+    }
+
 
     class Program
     {
-        static int MyDelegat(Element el1, Element el2)
+        public static void Main()
         {
-            if (el1.Ball>el2.Ball) return 1;
-            if (el1.Ball<el2.Ball) return -1;
-            return 0;
-        }
-        static void Main()
-        {
-            List<Element> list = new List<Element>();
-            using (StreamReader sr = new StreamReader("data.txt"))
-            {
-                int n=int.Parse(sr.ReadLine() ?? throw new InvalidOperationException());
-                for (int i=0; i<n; i++)
-                {
-                    string[] s=sr.ReadLine()?.Split(' ');
-                    int ball=int.Parse(s[2])+int.Parse(s[3])+int.Parse(s[4]);
-                    list.Add(new Element(s[0]+" "+s[1],ball));
-                }
-            }
-            list.Sort(MyDelegat);
-            foreach (var v in list)
-            {
-                Console.WriteLine($"{v.FIO,20}{v.Ball,10}");
-            }
-            Console.WriteLine();
-            int ball2=list[2].Ball;
-            foreach (var v in list)
-            {
-                if (v.Ball<=ball2)
-                    Console.WriteLine($"{v.FIO,20}{v.Ball,10}");
-            }
+            ExampleLinq exampleLinq = new ExampleLinq();
+            exampleLinq.Group();
+
+
+
             Console.ReadKey();
         }
     }
