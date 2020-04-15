c7e0621c905f8813193fd693a4c0fa61542c915b
kanadeiar <kanadeiar@gmail.com>
2020-04-15 19:54:57 +0300

    add user

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -5,13 +5,32 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-
+    public struct User
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public int Age {get;set;}
+        public User(string firstName, string lastName) : this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public void Deconstruct(out string firstName, out string lastName, out int age)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            age = Age;
+        }
+    }
 
 
     class Program
     {
         public static void Main()
         {
+            User person = new User("Andrey","Muratov") {Age=18};
+            (string fname, string lname, int age) = person;
+            Console.WriteLine($"{fname}\n{lname}\n{age}");
 
 
 
