df74abb345694ea291ea301ddfd967feff2484eb
kanadeiar <kanadeiar@gmail.com>
2020-04-19 11:10:22 +0300

    add button

--- CppConsoleApplications/WpfApp1HelloWPF/App.xaml.cs
+++ CppConsoleApplications/WpfApp1HelloWPF/App.xaml.cs
@@ -13,5 +13,11 @@ namespace WpfApp1HelloWPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+
+            wnd.Show();
+        }
     }
 }
