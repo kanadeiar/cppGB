efff560cdf9a6f4cc4591bd2201d6e3d20acb56b
kanadeiar <kanadeiar@gmail.com>
2020-04-27 23:53:56 +0300

    clear

--- CppConsoleApplications/WpfApp1HelloWPF/MainWindow.xaml.cs
+++ CppConsoleApplications/WpfApp1HelloWPF/MainWindow.xaml.cs
@@ -26,10 +26,5 @@ namespace WpfApp1HelloWPF
             InitializeComponent();
         }
     }
-    class Employee
-    {
-        public string Name {get;set;}
-        public int Age {get;set;}
-        public int Salary {get;set;}
-    }
+
 }
