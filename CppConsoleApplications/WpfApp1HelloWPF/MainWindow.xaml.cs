862b88ba15cf626db04170286f1d366d568434de
kanadeiar <kanadeiar@gmail.com>
2020-04-28 00:28:45 +0300

    converter

--- CppConsoleApplications/WpfApp1HelloWPF/MainWindow.xaml.cs
+++ CppConsoleApplications/WpfApp1HelloWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,5 +27,31 @@ namespace WpfApp1HelloWPF
             InitializeComponent();
         }
     }
-
+    public class YesNoToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch(value.ToString().ToLower())
+            {
+                case "yes":
+                case "true":
+                    return true;
+                case "no":
+                case "false":
+                    return false;
+            }
+            return false;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value==true)
+                    return "yes";
+                else
+                    return "no";
+            }
+            return "no";
+        }
+    }
 }
