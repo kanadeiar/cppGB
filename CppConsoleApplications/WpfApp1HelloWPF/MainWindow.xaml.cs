087abad258398a0d47d8802ad3bff5cc1b388473
kanadeiar <kanadeiar@gmail.com>
2020-05-03 19:58:48 +0300

    add ADO

--- CppConsoleApplications/WpfApp1HelloWPF/MainWindow.xaml.cs
+++ CppConsoleApplications/WpfApp1HelloWPF/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
+using System.Data.SqlClient;
 
 namespace WpfApp1HelloWPF
 {
@@ -24,11 +25,20 @@ namespace WpfApp1HelloWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        string count = "INSERT INTO [People] (FIO,Birthday,Email,Phone) VALUES ('Иванов Иван Иванович','01.01.2001','some@some.ru','89297771111');";
         public MainWindow()
         {
             InitializeComponent();
+            string connectionString = @"data source = DESKTOP-Q5PLE8H\SQLEXPRESS; Initial Catalog = Lesson7; Integrated Security = True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(count, connection);
+                int newId = Convert.ToInt32(command.ExecuteScalar());
+                MessageBox.Show($"Добавлена запись с ид: {newId}");
+                connection.Close();
+            }
         }
-
     }
 
 
