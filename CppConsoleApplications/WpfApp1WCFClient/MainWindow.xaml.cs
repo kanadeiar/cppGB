e0cc91784f2273c920f0416e22c591fdb6de0176
kanadeiar <kanadeiar@gmail.com>
2020-05-08 10:59:19 +0300

    complete service

---
+++ CppConsoleApplications/WpfApp1WCFClient/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1WCFClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void summButton_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient serviceClient = new ServiceReference1.WebService1SoapClient();
+            string sum = serviceClient.Summ(textBox1.Text,textBox2.Text);
+            resultTextBlock.Text = sum;
+        }
+    }
+}
