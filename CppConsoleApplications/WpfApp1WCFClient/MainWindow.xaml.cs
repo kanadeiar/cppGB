f7adfb9024922f18b7b4699f09f36136420212f4
kanadeiar <kanadeiar@gmail.com>
2020-05-13 22:08:32 +0300

    clean code

--- CppConsoleApplications/WpfApp1WCFClient/MainWindow.xaml.cs
+++ CppConsoleApplications/WpfApp1WCFClient/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,10 +22,32 @@ namespace WpfApp1WCFClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        static HttpClient client;
         public MainWindow()
         {
             InitializeComponent();
+            client = new HttpClient();
+            client.BaseAddress = new Uri(@"https://localhost:44326/");
         }
-
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var products = await GetProductsAsync(client.BaseAddress + "api/products");
+            productDataGrid.ItemsSource = products;
+        }
+        static async Task<IEnumerable<Product>> GetProductsAsync(string path)
+        {
+            IEnumerable<Product> products = null;
+            try
+            {
+                var response = await client.GetAsync(path);
+                response.EnsureSuccessStatusCode();
+                products = await response.Content.ReadAsAsync<IEnumerable<Product>>();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка!"+ex.Message);
+            }
+            return products;
+        }
     }
 }
