f74cff348e4ad980aad24a85bb32f0152da3e518
kanadeiar <kanadeiar@gmail.com>
2020-05-14 22:36:21 +0300

    dll complete

--- CppConsoleApplications/ConsoleApplication1/ConsoleApplication1.cpp
+++ CppConsoleApplications/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,8 @@ using namespace std;
 int main()
 {
 	Ru();
+
+
 	
 	//puts("Задача 1.");
 	//TRoad road(60, 3);
@@ -24,13 +26,13 @@ int main()
 	//}
 	//while (!_kbhit());
 	
-	TPen pen;
-	pen.setColor("FFFF00");
-	cout << "Цвет пера: " << pen.getColor() << endl;
+	//TPen pen;
+	//pen.setColor("FFFF00");
+	//cout << "Цвет пера: " << pen.getColor() << endl;
 
-	TNot n;
-	n.setIn1(false);
-	cout << "Результат: " << n.getRes() << endl;
+	//TNot n;
+	//n.setIn1(false);
+	//cout << "Результат: " << n.getRes() << endl;
 
 	//TNot elNot;
 	//TAnd elAnd;
@@ -47,18 +49,18 @@ int main()
 	//		cout << " " << A << " " << B << "    " << elNot.getRes() << endl;
 	//	}
 	//}
-	TNot elNot;
-	TAnd elAnd;
-	elAnd.Link(&elNot,1);
-	for (int A=0; A<=1; A++)
-	{
-		elAnd.setIn1(A);
-		for (int B=0; B<=1; B++)
-		{
-			elAnd.setIn2(B);
-			cout << " " << A << " " << B << "    " << elNot.getRes() << endl;
-		}
-	}
+	//TNot elNot;
+	//TAnd elAnd;
+	//elAnd.Link(&elNot,1);
+	//for (int A=0; A<=1; A++)
+	//{
+	//	elAnd.setIn1(A);
+	//	for (int B=0; B<=1; B++)
+	//	{
+	//		elAnd.setIn2(B);
+	//		cout << " " << A << " " << B << "    " << elNot.getRes() << endl;
+	//	}
+	//}
 
 	getchar();
 }
