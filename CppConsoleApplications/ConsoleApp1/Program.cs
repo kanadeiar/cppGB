f25127e9d30c6c872d47ae53dca59ec91615a5e3
kanadeiar <kanadeiar@gmail.com>
2020-05-14 13:39:58 +0300

    lazy

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace ConsoleApp1
@@ -9,12 +10,20 @@ namespace ConsoleApp1
 
     class Program
     {
+        class MyClass
+        {
+            private Lazy<String> name = new Lazy<String>(() => "новая строка");
+            public String GetName()
+            {
+                return name.Value; //значение с отложенной инициализацией
+            }
+        }
         public static void Main()
         {
-
-
-
-            Console.ReadKey();
+            MyClass my = new MyClass();
+            Console.WriteLine(my.GetName()); //здесь размещение объекта в памяти
+            Console.ReadLine();
         }
+
     }
 }
