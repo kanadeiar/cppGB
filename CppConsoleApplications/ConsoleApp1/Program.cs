465e1097af2a5f0286e0d7e2959e24043a8296aa
kanadeiar <kanadeiar@gmail.com>
2020-05-16 22:29:27 +0300

    clean code

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -6,17 +6,15 @@ using System.Linq;
 
 using ClassLibrary1;
 
+using System.Configuration;
+
 namespace ConsoleApp1
 {
     class Program
     {
         public static void Main()
         {
-            Worker worker = new Worker("Вася",18,5000);
-            worker.AddSalary();
-            Manager manager = new Manager("Петя",20,10000);
-            manager.AddSalary();
-            Console.WriteLine("Нажмите любую кнопку");
+
             Console.ReadLine();
         }
     }
