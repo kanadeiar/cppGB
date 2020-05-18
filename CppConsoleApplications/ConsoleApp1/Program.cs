456d79ea824ca577ad5f851a85a30c3bc8b52faf
kanadeiar <kanadeiar@gmail.com>
2020-05-18 13:13:26 +0300

    clear

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -16,32 +16,10 @@ namespace ConsoleApp1
     {
         public static void Main()
         {
-            Assembly a = null;
-            try
-            {
-                a = Assembly.Load("ClassLibrary1");
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine(e.Message); Console.ReadLine();
-                return;
-            }
-            if (a != null)
-            {
-                try
-                {
-                    var myClass = a.GetType("ClassLibrary1.Class1");
-                    var obj = Activator.CreateInstance(myClass);
-                    var mi = myClass.GetMethod("SetAge");
-                    mi.Invoke(obj, new object[]{19});
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message); Console.ReadLine();
-                    return;
-                }
-            }
+
             Console.ReadLine();
         }
     }
+
+
 }
