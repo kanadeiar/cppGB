458af92cf0bbbc5464d9e1c61fc3c51ae7ccf677
kanadeiar <kanadeiar@gmail.com>
2020-05-19 21:51:49 +0300

    add library

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Windows.Forms;
 
-
+using Excel = Microsoft.Office.Interop.Excel;
 namespace ConsoleApp1
 {
     class Program
     {
         public static void Main()
         {
-
-
-
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Workbooks.Add();
+            Excel.Worksheet worksheet = excelApp.ActiveSheet;
+            worksheet.Cells[1, "A"] = "Fam";
+            worksheet.Cells[2, "B"] = "Name";
+            worksheet.Cells[3, "C"] = "Age";
+            for (int i = 0; i <= 5; i++)
+            {
+                worksheet.Cells[i + 1, "A"] = $"Фамилия{i}";
+                worksheet.Cells[i + 1, "B"] = $"{i}-е имя";
+                worksheet.Cells[i + 1, "C"] = 18 + i;
+            }
+            worksheet.Range["A1"].AutoFormat(Excel.XlRangeAutoFormat.xlRangeAutoFormatClassic2);
+            excelApp.Visible = true;
+            worksheet.SaveAs($@"{Environment.CurrentDirectory}\Test.xlsx");
+            //excelApp.Quit();
             Console.ReadLine();
         }
     }
