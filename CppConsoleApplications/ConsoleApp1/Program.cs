914105d5a32cddaabd3213546d8d6b6ad60e8c85
kanadeiar <kanadeiar@gmail.com>
2020-05-20 10:35:51 +0300

    clean code

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -1,36 +1,20 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 
+
+using System.Runtime.Remoting.Contexts;
+using System.Threading;
 
 namespace ConsoleApp1
 {
+
     class Program
     {
         public static void Main()
         {
-            Process myProc = null;
-            try
-            {
-                ProcessStartInfo startInfo = new ProcessStartInfo("firefox.exe", "www.yandex.ru");
-                startInfo.WindowStyle = ProcessWindowStyle.Maximized;
-                myProc = Process.Start(startInfo);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return;
-            }
-            Console.WriteLine($"Нажмите клавишу для закрытия процесса '{myProc.ProcessName}'");
-            Console.ReadKey();
-            try
-            {
-                myProc.Kill();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+
             Console.ReadLine();
         }
     }
