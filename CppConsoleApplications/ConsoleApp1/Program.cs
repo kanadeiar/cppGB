b1f7e846e15d6948210cb3b85102d839cb7d6260
kanadeiar <kanadeiar@gmail.com>
2020-05-21 19:46:06 +0300

    add winform

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -1,44 +1,41 @@
 ﻿using System;
-using System.Runtime.Remoting.Contexts;
-using System.Runtime.Remoting.Messaging;
 using System.Threading;
 
 namespace ConsoleApp1
 {
-    public delegate int MySample(int x, int y);
     class Program
     {
-        private static bool isDone = false;
-        public static void Main()
+        static Random r = new Random();
+        class MyClass
         {
-            Console.WriteLine($"Main() запущен в потоке: {Thread.CurrentThread.ManagedThreadId}");
-            MySample mySmp = new MySample(Add);
-            //IAsyncResult asyncRes = mySmp.BeginInvoke(10, 10, null, null); //без передачи делегата
-            //IAsyncResult asyncRes = mySmp.BeginInvoke(10, 10, AddComplete, null); //передача делегата обратного вызова
-            IAsyncResult asyncRes = mySmp.BeginInvoke(10, 10, AddComplete, "Привет шахтерам!"); //передача делегата обратного вызова и данных
-            while (!isDone)
+            private object thredLock = new object(); //маркер блокировки
+            public void Print()
             {
-                Console.WriteLine("Ожидание выполнения в Main()");
-                Thread.Sleep(1000);
-            }
-            Console.ReadLine();
-            int Add(int x, int y)
-            {
-                Console.WriteLine($"Add() запущен в потоке {Thread.CurrentThread.ManagedThreadId}");
-                Thread.Sleep(5000);
-                return x + y;
-            }
-            void AddComplete(IAsyncResult iar)
-            {
-                Console.WriteLine($"Add() завершен в потоке {Thread.CurrentThread.ManagedThreadId}");
-                AsyncResult ar = (AsyncResult)iar;
-                MySample tmp = (MySample)ar.AsyncDelegate;
-                int answer = tmp.EndInvoke(ar);
-                Console.WriteLine($"Результат: 10 + 10 = {answer}");
-                string msg = (string) iar.AsyncState; //сообщение
-                Console.WriteLine(msg); //сообщение
-                isDone = true;
+                lock (thredLock) //использование маркера блокировки
+                {
+                    Console.WriteLine($"Поток \"{Thread.CurrentThread.Name}\" числа: ");
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.Write($"{i + 1},");
+                        Thread.Sleep(1000 * r.Next(3));
+                    }
+                    Console.Write("конец\n");
+                }
             }
         }
+        public static void Main()
+        {
+            MyClass myClass = new MyClass();
+            Thread[] threads = new Thread[10]; //10 потоков
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(myClass.Print));
+                threads[i].Name = $"Поток № {i+1}";
+            }
+            foreach (var th in threads)
+                th.Start();
+            Console.ReadLine();
+        }
+
     }
 }
