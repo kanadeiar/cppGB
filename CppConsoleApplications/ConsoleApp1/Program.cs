f9c0ba317b46c81ac2dd6ec598b243f30e213307
kanadeiar <kanadeiar@gmail.com>
2020-05-23 14:22:00 +0300

    clean

--- CppConsoleApplications/ConsoleApp1/Program.cs
+++ CppConsoleApplications/ConsoleApp1/Program.cs
@@ -1,42 +1,22 @@
 ﻿using System;
-using System.Runtime.Remoting.Contexts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        class MyClass
+        static void Main() //вызов в неблокирующей манере
         {
-            public int A { get; set; }
-            public int B { get; set; }
-            public MyClass(int a, int b)
-            {
-                this.A = a;
-                this.B = b;
-            }
-        }
-        public static void Main()
-        {
-            Console.WriteLine($"Работа Main() в потоке {Thread.CurrentThread.Name}");
-            MyClass myClass = new MyClass(2,2);
-            WaitCallback workItem = new WaitCallback(MyPrint); 
-            for (int i = 0; i < 10; i++)
-                ThreadPool.QueueUserWorkItem(workItem, myClass); //запуск метода в очередь 10 раз
-            Console.WriteLine("Нажать кнопку для завершения всех потоков");
-            Console.ReadLine();
-        }
-        static void MyPrint(object data)
-        {
-            if (data is MyClass myClass)
-            {
-                for (int i = 0; i < 20; i++)
-                {
-                    Console.WriteLine($"Поток {Thread.CurrentThread.Name}: {myClass.A} + {myClass.B} = {myClass.A + myClass.B}");
-                    myClass.A++;
-                    Thread.Sleep(1000);
-                }
-            }
+
+
+
+            Console.WriteLine("Нажмите любую кнопку ...");
+            Console.ReadKey();
         }
 
     }
--- CppConsoleApplications/WpfApp1/MainWindow.xaml.cs
+++ CppConsoleApplications/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Threading;
@@ -12,35 +13,51 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CancellationTokenSource cancelToken = new CancellationTokenSource(); //для отмены операции
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void CmdCancel_OnClick(object sender, RoutedEventArgs e)
-        {
-            //
-        }
         private void CmdProcess_OnClick(object sender, RoutedEventArgs e)
         {
-            ProcessFiles();
+            Task.Factory.StartNew(() => ProcessFiles()); //вызов метода во вторичном потоке
         }
         private void ProcessFiles()
         {
+            ParallelOptions parOpts = new ParallelOptions(); //опции для хранения токена отмены операции
+            parOpts.CancellationToken = cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism = Environment.ProcessorCount;
+
             string[] files = Directory.GetFiles(@".\Test", "*.jpg",SearchOption.AllDirectories);
             string newDir = @".\MidifiedTest";
             Directory.CreateDirectory(newDir);
 
-            Parallel.ForEach(files, curr =>
+            try //отмена оперции путем отслеживания исключения
             {
-                string filename = Path.GetFileName(curr);
-                using (Bitmap bitmap = new Bitmap(curr))
+                Parallel.ForEach(files, parOpts, curr =>
                 {
-                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
-                    bitmap.Save(Path.Combine(newDir, filename));
-                    //this.Title = $"Выполенено {filename} в потоке {Thread.CurrentThread.ManagedThreadId}";
-                }
-            });
-            this.Title = "Выполнено!";
+                    parOpts.CancellationToken.ThrowIfCancellationRequested(); //исключение если отмена операции
+
+                    string filename = Path.GetFileName(curr);
+                    using (Bitmap bitmap = new Bitmap(curr))
+                    {
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(newDir, filename));
+                        //this.Title = $"Выполенено {filename} в потоке {Thread.CurrentThread.ManagedThreadId}"; //не работает в многопоточной среде
+                        Thread.Sleep(1000);
+                        this.Dispatcher.Invoke(() => Debug.WriteLine($"Выполнено {filename}")); //безопасное в отношении потоков обновление интерфейса
+                    }
+                });
+                this.Dispatcher.Invoke(() => this.Title = $"Все Выполнено!"); //безопасное в отношении потоков обновление интерфейса
+            }
+            catch (OperationCanceledException ex)
+            {
+                this.Dispatcher.Invoke(() => Debug.WriteLine(ex.Message));
+            }
+        }
+        private void CmdCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            cancelToken.Cancel(); //для отмены операции
         }
     }
 }
